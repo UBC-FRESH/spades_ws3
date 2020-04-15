4f258d35bb39d62858db6f0d08cd75ba5b4368fc
Ian Eddy <ianmseddy@gmail.com>
2020-04-15 17:37:34 +0000

    changes to scheduling

--- spades_ws3.R
+++ spades_ws3.R
@@ -4,22 +4,24 @@
 # to all modules. Functions can be used without sim$ as they are namespaced, like functions
 # in R packages. If exact location is required, functions will be: sim$<moduleName>$FunctionName
 defineModule(sim, list(
-  name = "harvesting",
+  name = "spades_ws3",
   description = NA, #"insert module description here",
   keywords = NA, # c("insert key words here"),
   authors = c(person(c("First", "Middle"), "Last", email = "email@example.com", role = c("aut", "cre"))),
   childModules = character(0),
-  version = list(SpaDES.core = "0.2.5.9000", harvesting = "0.0.1"),
+  version = list(SpaDES.core = "0.2.5.9000", spades_ws3 = "0.0.1"),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
   citation = list("citation.bib"),
-  documentation = list("README.txt", "harvesting.Rmd"),
+  documentation = list("README.txt", "spades_ws3.Rmd"),
   reqdPkgs = list(),
   parameters = rbind(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description
-    defineParameter("basenames", "character", NA, NA, NA, "MU baseneames to load"),
+    defineParameter("basenames", "character", NA, NA, NA, 'MU baseneames to load, e.g. "tsa40"'),
+    defineParameter("base.year", 'numeric', 2015, NA, NA, 'base year of forest inventory data'),
     defineParameter("horizon", "numeric", NA, NA, NA, "ws3 simulation horizon (periods)"),
+    defineParameter("yearOfFirstHarvest", start(sim), NA, NA, "year to schedule first harvest"),
     defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
     defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between plot events"),
     defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
@@ -32,14 +34,17 @@ defineModule(sim, list(
   ),
   outputObjects = bind_rows(
     #createsOutput("objectName", "objectClass", "output object description", ...),
-    createsOutput(objectName = NA, objectClass = NA, desc = NA)
+    createsOutput("landscape", objectClass = "RasterStack",
+    desc = 'stack containing FMU, THLB, AU, Block ID, and stand age'),
+    createsOutput(objectName = 'rstCurrentHarvest', objectClass = 'RasterLayer',
+                  desc = 'a binary raster representing harvest locations at time(sim)')
   )
 ))
 
 ## event types
 #   - type `init` is required for initialization
 
-doEvent.harvesting = function(sim, eventTime, eventType) {
+doEvent.spades_ws3 = function(sim, eventTime, eventType) {
   switch(
     eventType,
     init = {
@@ -50,45 +55,29 @@ doEvent.harvesting = function(sim, eventTime, eventType) {
       sim <- Init(sim)
 
       # schedule future event(s)
-      sim <- scheduleEvent(sim, 0, "harvesting", "harvest")
-      sim <- scheduleEvent(sim, 0, "harvesting", "grow")
-      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "harvesting", "plot")
-      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "harvesting", "save")
+      sim <- scheduleEvent(sim, P(sim)$yearOfFirstHarvest, "spades_ws3", "harvest")
+      sim <- scheduleEvent(sim, P(sim)$yearOfFirstHarvest, "spades_ws3", "grow")
+      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "spades_ws3", "plot")
+      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "spades_ws3", "save")
+
     },
     plot = {
-      # ! ----- EDIT BELOW ----- ! #
-      # do stuff for this event
 
-      #plotFun(sim) # uncomment this, replace with object to plot
-      # schedule future event(s)
-
-      # e.g.,
-      #sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "harvesting", "plot")
-
-      # ! ----- STOP EDITING ----- ! #
     },
     save = {
-      # ! ----- EDIT BELOW ----- ! #
-      # do stuff for this event
 
-      # e.g., call your custom functions/methods here
-      # you can define your own methods below this `doEvent` function
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$.saveInterval, "spades_ws3", "save")
 
-      # schedule future event(s)
-
-      # e.g.,
-      sim <- scheduleEvent(sim, time(sim) + P(sim)$.saveInterval, "harvesting", "save")
-
-      # ! ----- STOP EDITING ----- ! #
     },
     harvest = {
-      sim <- applyHarvest(sim) 
-      sim <- scheduleEvent(sim, time(sim) + 1, "harvesting", "harvest")
+      sim <- applyHarvest(sim)
+      sim <- scheduleEvent(sim, time(sim) + 1, "spades_ws3", "harvest")
     },
     grow = {
       sim <- applyGrow(sim)
-      sim <- scheduleEvent(sim, time(sim) + 1, "harvesting", "grow")
+      sim <- scheduleEvent(sim, time(sim) + 1, "spades_ws3", "grow")
     },
+
     warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
                   "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
   )
@@ -105,7 +94,7 @@ Init <- function(sim) {
   py$sys$path <- insert(py$sys$path, 1, file.path(modulePath(sim), currentModule(sim), "python", "ws3"))
   py$basenames <- params(sim)$.globals$basenames
   py_run_file(file.path(modulePath(sim), currentModule(sim), "python", "spadesws3_params.py"))
-  py$base_year <- params(sim)$.globals$base.year
+  py$base_year <- P(sim)$base.year
   py$horizon <- P(sim, module=currentModule(sim))$horizon
   sim$fm <- py$bootstrap_forestmodel_kwargs()
   return(invisible(sim))
@@ -134,25 +123,25 @@ plotFun <- function(sim) {
 
 updateAges <- function(sim) {
   #browser()
-  year <- as.integer(time(sim) + params(sim)$.globals$base.year)
+  year <- as.integer(time(sim) - start(sim) + P(sim)$base.year)
   files <- sapply(P(sim, module = currentModule(sim))$basenames,
                   function(bn) file.path(inputPath(sim),
                                          P(sim, module = currentModule(sim))$tifPath,
                                          bn,
                                          paste("inventory_", toString(year), ".tif", sep="")))
   rs.list <- sapply(files, stack) # one stack per MU
-  rs.list <- rapply(rs.list, 
+  rs.list <- rapply(rs.list,
                     function(rs) {
                       rs[[2]] <- mask(crop(sim$landscape$age, rs[[2]]), rs[[2]])
                       rs[[2]][is.nan(rs[[2]])] <- NA
                       return(rs)})
-  mapply(writeRaster, rs.list, files, format='GTiff', overwrite=TRUE, datatype='INT4S') 
+  mapply(writeRaster, rs.list, files, format='GTiff', overwrite=TRUE, datatype='INT4S')
   return(invisible(sim))
 }
 
 loadAges <- function(sim) {
   #browser()
-  year <- as.integer(time(sim) + params(sim)$.globals$base.year)
+  year <- as.integer(time(sim) - start(sim) + P(sim)$base.year)
   files <- sapply(P(sim, module = currentModule(sim))$basenames,
                   function(bn) file.path(inputPath(sim),
                                          P(sim, module = currentModule(sim))$tifPath,
@@ -169,18 +158,19 @@ loadAges <- function(sim) {
 
 applyHarvest <- function(sim) {
   #browser()
-  year <- as.integer(time(sim) + params(sim)$.globals$base.year)
+  year <- as.integer(time(sim) - start(sim) + P(sim)$base.year)
   py$base_year <- year
   sim$fm$base_year <- year
   #py$schedule_harvest_kwargslist(sim$fm)
   updateAges(sim)
+
   py$simulate_harvest(sim$fm, py$basenames, year) # run aspatial scheduler and allocate to pixels
   sim$landscape$age <- loadAges(sim)
   return(invisible(sim))
 }
 
 applyGrow <- function(sim) {
-  sim$landscape$age <- sim$landscape$age + 1 
+  sim$landscape$age <- sim$landscape$age + 1
   return(invisible(sim))
 }
 
